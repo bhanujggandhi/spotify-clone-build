5d4503fd8269162583634ed304ec31b7272c55dd
bhanujggandhi <gandhibhanuj@gmail.com>
2022-02-12 22:02:19 +0530

    feat/authentication added

--- components/Siderbar.tsx
+++ components/Siderbar.tsx
@@ -6,12 +6,23 @@ import {
   PlusCircleIcon,
   HeartIcon,
   RssIcon,
+  LogoutIcon,
 } from '@heroicons/react/outline'
+import { signOut, useSession } from 'next-auth/react'
 
 function Siderbar() {
+  const { data: session, status } = useSession()
+
   return (
     <div className="border-r border-gray-900 p-5 text-sm text-gray-500">
       <div className="space-y-4">
+        <button
+          onClick={() => signOut()}
+          className="flex items-center space-x-2 hover:text-white"
+        >
+          <LogoutIcon className="h-5 w-5" />
+          <p>Logout</p>
+        </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
