d0bb352dcf4b85909454bfe089733dab509be107
bhanujggandhi <gandhibhanuj@gmail.com>
2022-02-12 14:56:21 +0530

    style: Sidebar styled

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Siderbar from '../components/Siderbar'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="h-screen overflow-hidden bg-black">
       <main>
         <Siderbar />
         {/* Center */}
